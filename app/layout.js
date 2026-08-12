ab18736af5b9a9619ba3964d0b01740d1ff7b636
Kayonfire <kayafterdark@gmail.com>
2026-08-12 02:40:49 -0700

    Add POIDH Quest metadata

---
+++ app/layout.js
@@ -0,0 +1,1 @@
+export const metadata={title:'POIDH Quest',description:'Discover and complete real POIDH quests'};export default function Layout({children}){return <html><body>{children}</body></html>}
